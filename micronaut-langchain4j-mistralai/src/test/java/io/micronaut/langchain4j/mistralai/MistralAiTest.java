462e6c25dc6d6b95d40a8420c42f9369559b8c0d
Graeme Rocher <graeme.rocher@oracle.com>
2024-06-21 10:43:11 +0200

    fix docs

--- micronaut-langchain4j-mistralai/src/test/java/io/micronaut/langchain4j/mistralai/MistralAiTest.java
+++ micronaut-langchain4j-mistralai/src/test/java/io/micronaut/langchain4j/mistralai/MistralAiTest.java
@@ -5,14 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import dev.langchain4j.model.mistralai.MistralAiChatModel;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.annotation.Property;
-import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
 @Property(name = "langchain4j.mistral-ai.api-key", value = "blah")
-public class MistalAiTest {
+public class MistralAiTest {
 
     @Inject
     DefaultMistralAiChatModelConfiguration chatLanguageModelConfiguration;
