656edc6faf03f988e42d1b67aa87d30a09cb791f
Graeme Rocher <graeme.rocher@oracle.com>
2024-06-27 08:20:04 +0200

    add neo4j vector store

---
+++ micronaut-langchain4j-store-neo4j/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("io.micronaut.build.internal.langchain4j-module")
+    id("io.micronaut.test-resources")
+}
+
+dependencies {
+    annotationProcessor(projects.micronautLangchain4jProcessor)
+    annotationProcessor(mn.micronaut.inject.java)
+    annotationProcessor(mnSourcegen.micronaut.sourcegen.generator.java)
+    api(projects.micronautLangchain4jCore)
+    api(mnNeo4j.micronaut.neo4j.bolt)
+    implementation(libs.langchain4j.neo4j)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+    testImplementation(mnTest.micronaut.test.junit5)
+    testImplementation(mnSerde.micronaut.serde.jackson)
+    testImplementation(libs.langchain4j.embeddings.all.minilm.l6.v2)
+    testRuntimeOnly(mnLogging.logback.classic)
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
--- micronaut-langchain4j-store-neo4j/src/test/java/io/micronaut/langchain4j/neo4j/Neo4jTest.java
+++ micronaut-langchain4j-store-neo4j/src/test/java/io/micronaut/langchain4j/neo4j/Neo4jTest.java
@@ -1,4 +1,4 @@
-package io.micronaut.langchain4j.pgvector;
+package io.micronaut.langchain4j.neo4j;
 
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
@@ -6,18 +6,28 @@ import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
-import dev.langchain4j.store.embedding.pgvector.PgVectorEmbeddingStore;
+import dev.langchain4j.store.embedding.neo4j.Neo4jEmbeddingStore;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.neo4j.driver.Driver;
 
-@MicronautTest(transactional = false)
-public class PgVectorEmbeddingStoreTest {
+@MicronautTest
+public class Neo4jTest {
+    @Inject
+    Driver driver;
+
+    @Test
+    void testDriver(Driver driver) {
+        Assertions.assertNotNull(driver);
+    }
+
     @Test
     void testEmbeddingStore(EmbeddingStore<TextSegment> embeddingStore) {
         Assertions.assertNotNull(embeddingStore);
-        Assertions.assertInstanceOf(PgVectorEmbeddingStore.class, embeddingStore);
+        Assertions.assertInstanceOf(Neo4jEmbeddingStore.class, embeddingStore);
         EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
 
         TextSegment segment1 = TextSegment.from("I like football.");
