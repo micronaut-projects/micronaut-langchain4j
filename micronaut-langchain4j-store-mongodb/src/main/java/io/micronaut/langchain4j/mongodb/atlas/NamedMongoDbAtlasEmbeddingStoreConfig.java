e1d7f4db46d8f06291f608a9931b49170a570ec4
Graeme Rocher <graeme.rocher@oracle.com>
2024-06-28 15:55:30 +0200

    WIP mongodb support

---
+++ micronaut-langchain4j-store-mongodb/src/main/java/io/micronaut/langchain4j/mongodb/atlas/NamedMongoDbAtlasEmbeddingStoreConfig.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.langchain4j.mongodb.atlas;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.model.CreateCollectionOptions;
+import dev.langchain4j.store.embedding.mongodb.IndexMapping;
+import dev.langchain4j.store.embedding.mongodb.MongoDbEmbeddingStore;
+import io.micronaut.context.annotation.ConfigurationBuilder;
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.EachProperty;
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.core.annotation.Nullable;
+import java.util.Set;
+
+/**
+ * Configures for PgVectorEmbeddingStore.
+ */
+@EachProperty(value = NamedMongoDbAtlasEmbeddingStoreConfig.PREFIX, primary = "default")
+public class NamedMongoDbAtlasEmbeddingStoreConfig {
+    public static final String PREFIX = "langchain4j.mongodb-atlas.embedding-stores";
+    @ConfigurationBuilder(
+        prefixes = "",
+        excludes = { "fromClient", "mongoClient", "filter", "createCollectionOptions", "indexMapping"}
+    )
+    final MongoDbEmbeddingStore.Builder builder;
+    private final MongoClient mongoClient;
+
+    public NamedMongoDbAtlasEmbeddingStoreConfig(
+        @Parameter MongoClient mongoClient,
+        @Parameter @Nullable IndexMappingConfig indexMappingConfig,
+        @Parameter @Nullable CreateCollectionOptions createCollectionOptions) {
+        this.mongoClient = mongoClient;
+        this.builder = MongoDbEmbeddingStore.builder()
+            .fromClient(mongoClient)
+            .indexMapping(indexMappingConfig != null ? indexMappingConfig.indexMapping.build() : null)
+            .createCollectionOptions(createCollectionOptions);
+    }
+
+    /**
+     * @return The data source.
+     */
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    /**
+     * @return The embedded store builder.
+     */
+    public MongoDbEmbeddingStore.Builder getBuilder() {
+        return builder;
+    }
+
+    /**
+     * index mapping configuration.
+     */
+    @ConfigurationProperties("index-mapping")
+    static class IndexMappingConfig {
+        @ConfigurationBuilder(prefixes = "")
+        final IndexMapping.IndexMappingBuilder indexMapping;
+
+        public IndexMappingConfig() {
+            this.indexMapping = IndexMapping.builder().dimension(1536).metadataFieldNames(Set.of());
+        }
+    }
+}
---
+++ micronaut-langchain4j-store-mongodb/src/test/java/io/micronaut/langchain4j/mongodb/atlas/MongoStoreTest.java
@@ -0,0 +1,40 @@
+package io.micronaut.langchain4j.mongodb.atlas;
+
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.mongodb.MongoDbEmbeddingStore;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest(transactional = false)
+@Disabled("Investigate MongoDB Atlas Support in Testcontainers")
+public class MongoStoreTest {
+    @Test
+    void testEmbeddingStore(EmbeddingStore<TextSegment> embeddingStore) {
+        Assertions.assertNotNull(embeddingStore);
+        Assertions.assertInstanceOf(MongoDbEmbeddingStore.class, embeddingStore);
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        TextSegment segment1 = TextSegment.from("I like football.");
+        Embedding embedding1 = embeddingModel.embed(segment1).content();
+        embeddingStore.add(embedding1, segment1);
+
+        TextSegment segment2 = TextSegment.from("The weather is good today.");
+        Embedding embedding2 = embeddingModel.embed(segment2).content();
+        embeddingStore.add(embedding2, segment2);
+
+        Embedding queryEmbedding = embeddingModel.embed("What is your favourite sport?").content();
+        List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 1);
+        EmbeddingMatch<TextSegment> embeddingMatch = relevant.get(0);
+
+        System.out.println(embeddingMatch.score()); // 0.8144288608390052
+        Assertions.assertEquals("I like football.", embeddingMatch.embedded().text());
+    }
+}
