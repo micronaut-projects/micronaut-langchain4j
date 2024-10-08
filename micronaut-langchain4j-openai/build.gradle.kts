f405bfb3b2244d00545196bed43569aae83d5486
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-08 12:25:21 +0200

    openai

--- micronaut-langchain4j-openai/build.gradle.kts
+++ micronaut-langchain4j-openai/build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
 
 dependencies {
     implementation(libs.langchain4j.open.ai)
+    testImplementation(projects.testSuiteChatmodelTck)
+    testImplementation(libs.junit.platform.engine)
 }
