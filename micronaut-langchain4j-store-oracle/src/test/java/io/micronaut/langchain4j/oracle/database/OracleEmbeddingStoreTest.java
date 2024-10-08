cef0036cd74377d782b3f83a42cbd5e45de15903
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-08 12:25:04 +0200

    disable test on macos aarch64

--- micronaut-langchain4j-store-oracle/src/test/java/io/micronaut/langchain4j/oracle/database/OracleEmbeddingStoreTest.java
+++ micronaut-langchain4j-store-oracle/src/test/java/io/micronaut/langchain4j/oracle/database/OracleEmbeddingStoreTest.java
@@ -7,6 +7,8 @@ import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2Embedding
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.oracle.OracleEmbeddingStore;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.core.type.Argument;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -14,29 +16,31 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
-@MicronautTest(transactional = false)
 class OracleEmbeddingStoreTest {
 
-    @DisabledOnOs(value = OS.MAC, architectures = "arm64")
+    @DisabledOnOs(value = OS.MAC, architectures = "aarch64")
     @Test
-    void testEmbeddingStore(EmbeddingStore<TextSegment> embeddingStore) {
-        Assertions.assertNotNull(embeddingStore);
-        Assertions.assertInstanceOf(OracleEmbeddingStore.class, embeddingStore);
-        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+    void testEmbeddingStore() {
+        try (ApplicationContext applicationContext = ApplicationContext.run()) {
+            EmbeddingStore<TextSegment> embeddingStore = applicationContext.getBean(Argument.of(EmbeddingStore.class, TextSegment.class));
+            Assertions.assertNotNull(embeddingStore);
+            Assertions.assertInstanceOf(OracleEmbeddingStore.class, embeddingStore);
+            EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
 
-        TextSegment segment1 = TextSegment.from("I like football.");
-        Embedding embedding1 = embeddingModel.embed(segment1).content();
-        embeddingStore.add(embedding1, segment1);
+            TextSegment segment1 = TextSegment.from("I like football.");
+            Embedding embedding1 = embeddingModel.embed(segment1).content();
+            embeddingStore.add(embedding1, segment1);
 
-        TextSegment segment2 = TextSegment.from("The weather is good today.");
-        Embedding embedding2 = embeddingModel.embed(segment2).content();
-        embeddingStore.add(embedding2, segment2);
+            TextSegment segment2 = TextSegment.from("The weather is good today.");
+            Embedding embedding2 = embeddingModel.embed(segment2).content();
+            embeddingStore.add(embedding2, segment2);
 
-        Embedding queryEmbedding = embeddingModel.embed("What is your favourite sport?").content();
-        List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 1);
-        EmbeddingMatch<TextSegment> embeddingMatch = relevant.get(0);
+            Embedding queryEmbedding = embeddingModel.embed("What is your favourite sport?").content();
+            List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(queryEmbedding, 1);
+            EmbeddingMatch<TextSegment> embeddingMatch = relevant.get(0);
 
-        System.out.println(embeddingMatch.score()); // 0.8144288608390052
-        Assertions.assertEquals("I like football.", embeddingMatch.embedded().text());
+            System.out.println(embeddingMatch.score()); // 0.8144288608390052
+            Assertions.assertEquals("I like football.", embeddingMatch.embedded().text());
+        }
     }
 }
