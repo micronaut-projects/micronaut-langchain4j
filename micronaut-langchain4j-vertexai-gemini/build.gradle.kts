77d09107dfa2af86b4e5f0f1eed66ce78bd0d957
Graeme Rocher <graeme.rocher@oracle.com>
2025-02-11 11:35:57 -0400

    use constraints Gradle syntax

--- micronaut-langchain4j-vertexai-gemini/build.gradle.kts
+++ micronaut-langchain4j-vertexai-gemini/build.gradle.kts
@@ -4,9 +4,10 @@ plugins {
 
 dependencies {
     implementation(libs.langchain4j.vertex.ai.gemini)
-    // apply com.google.protobuf:protobuf-java directly because the version brought transitively contains a vulnerable version.
-    implementation(mnGrpc.protobuf.java)
-
     // TODO: remove when non-vulnerable version released
-    runtimeOnly(libs.threetenbp)
+    constraints {
+        // apply com.google.protobuf:protobuf-java directly because the version brought transitively contains a vulnerable version.
+        implementation(mnGrpc.protobuf.java)
+        implementation(libs.threetenbp)
+    }
 }
