421e8835cf3cc23d79b2da87009317bf9f55298b
Graeme Rocher <graeme.rocher@oracle.com>
2025-02-11 12:15:45 -0400

    address threeten transitive vulnerability (#96)
    * address threeten transitive vulnerability
    
    * upgrade core
    
    * use constraints Gradle syntax
    
    * fix version
    
    * use runtimeOnly instead of constraints

--- micronaut-langchain4j-vertexai/build.gradle.kts
+++ micronaut-langchain4j-vertexai/build.gradle.kts
@@ -4,5 +4,8 @@ plugins {
 
 dependencies {
     implementation(libs.langchain4j.vertex.ai)
-    implementation(mnGrpc.protobuf.java) // apply com.google.protobuf:protobuf-java directly because the version brought transitively contains a vulnerable version.
+    // TODO: remove when non-vulnerable version released
+    // apply com.google.protobuf:protobuf-java directly because the version brought transitively contains a vulnerable version.
+    runtimeOnly(mnGrpc.protobuf.java)
+    runtimeOnly(libs.threetenbp)
 }
