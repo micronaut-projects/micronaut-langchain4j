83bc8da6f8754f9a776d69a790c2910c625d4109
Sergio del Amo <sergio.delamo@softamo.com>
2025-07-16 11:59:27 +0200

    extract OllamaTestPropertyProvider

--- test-suite/src/test/java/example/micronaut/AssistantWithMemoryTest.java
+++ test-suite/src/test/java/example/micronaut/AssistantWithMemoryTest.java
@@ -1,30 +1,13 @@
 package example.micronaut;
 
-import dev.langchain4j.memory.ChatMemory;
-import io.micronaut.context.exceptions.ConfigurationException;
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.langchain4j.testutils.OllamaUtils;
-import io.micronaut.test.support.TestPropertyProvider;
+import io.micronaut.langchain4j.testutils.OllamaTestPropertyProvider;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public abstract class AssistantWithMemoryTest implements TestPropertyProvider {
-    @Override
-    public @NonNull Map<String, String> getProperties() {
-        Map<String, String> result = new HashMap<>();
-        try {
-            result.put("langchain4j.ollama.base-url", OllamaUtils.ollamaContainerBaseUrl());
-        } catch (Exception e) {
-            throw new ConfigurationException("Could not set Ollama base URL", e);
-        }
-        return result;
-    }
+public abstract class AssistantWithMemoryTest implements OllamaTestPropertyProvider {
 
     //tag::test[]
     @Test
