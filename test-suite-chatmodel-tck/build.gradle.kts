7258a3fb4f277cc639f861d7ddfe1c73f0f31e63
issam liqali <liqaliissam@gmail.com>
2025-11-05 15:13:08 +0100

    update build

--- test-suite-chatmodel-tck/build.gradle.kts
+++ test-suite-chatmodel-tck/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("io.micronaut.build.internal.langchain4j-java-library")
+    id ("io.micronaut.build.internal.java-base")
 }
 dependencies {
     implementation(projects.micronautLangchain4jCore)
