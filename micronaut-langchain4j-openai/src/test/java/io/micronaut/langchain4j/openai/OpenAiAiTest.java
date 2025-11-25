84d370c07d57f5c1e2f4ec91db93cd82a018c8be
Sergio del Amo <sergio.delamo@softamo.com>
2025-11-25 07:26:47 +0100

    remove public modifier

--- micronaut-langchain4j-openai/src/test/java/io/micronaut/langchain4j/openai/OpenAiAiTest.java
+++ micronaut-langchain4j-openai/src/test/java/io/micronaut/langchain4j/openai/OpenAiAiTest.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
     named = "LANGCHAIN4J_OPEN_AI_API_KEY",
     matches = "\\.+"
 )
-public class OpenAiAiTest {
+class OpenAiAiTest {
 
     @Inject
     DefaultOpenAiChatModelConfiguration chatLanguageModelConfiguration;
