32127efbcb2b8f6ad53cf4b42dbed9d68f61d9cb
Sergio del Amo <sergio.delamo@softamo.com>
2026-02-16 12:08:56 +0100

    revert @Suite change

--- micronaut-langchain4j-vertexai/src/test/java/io/micronaut/langchain4j/vertexai/VertexAiSuite.java
+++ micronaut-langchain4j-vertexai/src/test/java/io/micronaut/langchain4j/vertexai/VertexAiSuite.java
@@ -5,11 +5,10 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
-//@Suite
+@Suite
 @SelectPackages("io.micronaut.langchain4j.chatmodels.tck.tests")
 @SuiteDisplayName("ChatModels Test Compatibility Kit for the Vertex AI PaLM 2 implementation")
 @ExcludeClassNamePatterns({
-    "io.micronaut.langchain4j.chatmodels.tck.tests.ChatLanguageModelTest",
         "io.micronaut.langchain4j.chatmodels.tck.tests.StreamingChatLanguageModelTest", // no bean of type StreamingChatLanguage.
         "io.micronaut.langchain4j.chatmodels.tck.tests.AiServiceTest"  // com.google.api.gax.rpc.InvalidArgumentException: io.grpc.StatusRuntimeException: INVALID_ARGUMENT: Invalid instance. Missing required messages field. fields 	 {key: "content"value {string_value: "Hello"}}
 })
