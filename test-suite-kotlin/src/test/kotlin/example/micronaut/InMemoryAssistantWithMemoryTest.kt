ac4f8e23710bcfeb0dbf7e55a7daa695b94f6d4e
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-04 14:37:14 +0100

    disabled in CI

--- test-suite-kotlin/src/test/kotlin/example/micronaut/InMemoryAssistantWithMemoryTest.kt
+++ test-suite-kotlin/src/test/kotlin/example/micronaut/InMemoryAssistantWithMemoryTest.kt
@@ -2,8 +2,10 @@ package example.micronaut
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 import org.testcontainers.junit.jupiter.Testcontainers
 
+@DisabledIfEnvironmentVariable(named = "CI", matches = ".*")
 @Testcontainers(disabledWithoutDocker = true)
 @MicronautTest(startApplication = false)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
