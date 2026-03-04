6943500f7df1af0e3c9fc42809824ffd2bf6aa06
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-04 09:50:56 +0100

    change regex pattern for EnabledIfEnvironmentVariable

--- doc-examples/example-openai-java/src/test/java/example/openai/ToolTest.java
+++ doc-examples/example-openai-java/src/test/java/example/openai/ToolTest.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 @MicronautTest
 @EnabledIfEnvironmentVariable(
     named = "LANGCHAIN4J_OPEN_AI_API_KEY",
-    matches = "\\.+"
+    matches = ".+"
 )
 public class ToolTest {
     @Inject
