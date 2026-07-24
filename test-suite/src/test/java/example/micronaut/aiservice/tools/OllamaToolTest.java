74fe5ee21b1c8571f4f9f2bab74ab21bbf947656
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-07-24 11:33:58 +0200

    DisabledIfEnvironmentVariable

--- test-suite/src/test/java/example/micronaut/aiservice/tools/OllamaToolTest.java
+++ test-suite/src/test/java/example/micronaut/aiservice/tools/OllamaToolTest.java
@@ -8,12 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Property(name = "langchain4j.ollama.model-name", value = OllamaUtils.TOOL_MODEL_NAME)
 @Testcontainers(disabledWithoutDocker = true)
 @MicronautTest(startApplication = false)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisabledIfEnvironmentVariable(named = "CI", matches = ".*")
 class OllamaToolTest implements OllamaTestPropertyProvider {
     @Test
     void testInjectTools(CompanyBot bot) {
